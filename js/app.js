0f30f785f8b8a30fd15656abdbed36faaa61b64a
Pavol Daniš <palto@palto.sk>
2012-06-10 19:25:27 +0200

    Updated

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@
 
     app.controller('puzzleCtrl', function($scope) {
         $scope.puzzles = [
-            { id: 'angular', title: 'AngularJS', rows: 3, cols: 3 },
-            { id: 'chrome', title: 'Google Chrome', rows: 3, cols: 3 },
-            { id: 'firefox', title: 'Mozilla Firefox', rows: 3, cols: 3 },
-            { id: 'ie', title: 'Internet Explorer', rows: 3, cols: 3 }
+            { src: './img/angular.png', title: 'AngularJS', rows: 3, cols: 3 },
+            { src: './img/chrome.png', title: 'Google Chrome', rows: 4, cols: 4 },
+            { src: './img/firefox.png', title: 'Mozilla Firefox', rows: 5, cols: 5 },
+            { src: './img/ie.png', title: 'Internet Explorer', rows: 3, cols: 4 }
         ];
     });
 
